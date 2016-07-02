ee4eaa35b5640d0e4395ad02a505f86093551df6
Paul Ebreo <pebre79@hotmail.com>
2016-07-01 20:36:19 -0500

    remove old code and rewrite panel descriptions titles

--- js/app.js
+++ js/app.js
@@ -24,11 +24,6 @@ app.directive('comboElem', function () {
 
 app.controller('MyCtrl', ['$scope', '$log', function ($scope, $log) {
 
-    $scope.cols = 1;
-    $scope.item_count = 1;
-    $scope.slot_count = 1;
-    $scope.permutations_count = 1;
-    $scope.combinations_count = 1;
 
     $scope.item_names = {
         1: {'button_type': 'btn-primary', 'text': 'One', 'sqcolor': 'red'},
@@ -37,7 +32,6 @@ app.controller('MyCtrl', ['$scope', '$log', function ($scope, $log) {
         4: {'button_type': 'btn-success', 'text': 'Four', 'sqcolor': 'beige'},
         5: {'button_type': 'btn-danger', 'text': 'Five', 'sqcolor': 'purple'}
     };
-    $scope.good_items = [];
 
     $scope.get_good_items = function () {
         var items = [];
@@ -88,134 +82,23 @@ app.controller('MyCtrl', ['$scope', '$log', function ($scope, $log) {
 
     };
 
-     $scope.add_slot = function () {
-        $log.log('add');
-        if ($scope.slot_count < $scope.item_count) {
-            $scope.slot_count += 1;
-        }
-
-    };
-
-    $scope.decrease_slot = function () {
-        $log.log('remove');
-        if ($scope.slot_count > 1) {
-            $scope.slot_count -= 1;
-        }
-
-    };
-
-    $scope.create = function () {
-        //alert($scope.item_count);
-        //$log.log($scope.item_count);
-        var b = _.join(['a', 'b']);
-        //$log.log(b);
-    };
-
-
-    $scope.permutator = function (inputArr) {
-        var results = [];
-
-        function permute(arr, memo) {
-            var cur, memo = memo || [];
-
-            for (var i = 0; i < arr.length; i++) {
-                cur = arr.splice(i, 1);
-                if (arr.length === 0) {
-                    results.push(memo.concat(cur));
-                }
-                permute(arr.slice(), memo.concat(cur));
-                arr.splice(i, 0, cur[0]);
-            }
-
-            return results;
-        }
-
-        return permute(inputArr);
-    };
-
-    $scope.combinations = function (list) {
-        var set = [],
-            listSize = list.length,
-            combinationsCount = (1 << listSize),
-            combination;
-
-        for (var i = 1; i < combinationsCount; i++) {
-            var combination = [];
-            for (var j = 0; j < listSize; j++) {
-                if ((i & (1 << j))) {
-                    combination.push(list[j]);
-                }
-            }
-            set.push(combination);
-        }
-        return set;
-    };
-
-    var permutations_choose = function(xs, r)
-    {
-        if (!r) return [];
-        return xs.reduce(function (memo, cur, i) {
-            var others = xs.slice(0, i).concat(xs.slice(i + 1)),
-                perms = permutations_choose(others, r - 1),
-                newElms = !perms.length ? [[cur]] :
-                    perms.map(function (perm) {
-                        return [cur].concat(perm)
-                    });
-            return memo.concat(newElms);
-        }, []);
-    };
-
-    $scope.combinations_choose = function (list, r) {
-        var combos = $scope.combinations(list);
-        var results =  _.filter(combos, function(item) { return item.length == r;});
-        return results;
-    };
-
-    $scope.perm_set_items = [];
-    $scope.perm_choose_list = [];
-
-    $scope.combo_set_items = [];
-    $scope.combo_choose_list = [];
-
-
-    var update_ = function() {
-
-    };
-
-    $scope.$watch('item_count', function(){
-        $scope.perm_set_items = $scope.range(1, $scope.item_count);
-        $scope.perm_choose_list = permutations_choose($scope.perm_set_items, $scope.slot_count);
-        $scope.permutations_count = $scope.perm_choose_list.length;
-
-        $scope.combo_choose_list = [];
-        $scope.combo_set_items = $scope.range(1, $scope.item_count);
-        $scope.combo_choose_list = $scope.combinations_choose($scope.combo_set_items, $scope.slot_count);
-        $scope.combinations_count = $scope.combo_choose_list.length;
-
-    });
-
-    $scope.$watch('slot_count', function(){
-        $scope.perm_set_items = $scope.range(1, $scope.item_count);
-        $scope.perm_choose_list = permutations_choose($scope.perm_set_items, $scope.slot_count);
-        $scope.permutations_count = $scope.perm_choose_list.length;
-
-        $scope.combo_choose_list = [];
-        $scope.combo_set_items = $scope.range(1, $scope.item_count);
-        $scope.combo_choose_list = $scope.combinations_choose($scope.combo_set_items, $scope.slot_count);
-        $scope.combinations_count = $scope.combo_choose_list.length;
-
-    });
 
 
 
-    $scope.get_perms = function () {
-        return $scope.perm_choose_list;
-    };
+
+    //$scope.$watch('item_count', function(){
+    //    $scope.perm_set_items = $scope.range(1, $scope.item_count);
+    //    $scope.perm_choose_list = permutations_choose($scope.perm_set_items, $scope.slot_count);
+    //    $scope.permutations_count = $scope.perm_choose_list.length;
+    //
+    //    $scope.combo_choose_list = [];
+    //    $scope.combo_set_items = $scope.range(1, $scope.item_count);
+    //    $scope.combo_choose_list = $scope.combinations_choose($scope.combo_set_items, $scope.slot_count);
+    //    $scope.combinations_count = $scope.combo_choose_list.length;
+    //
+    //});
 
 
-    $scope.get_combos = function() {
-        return $scope.combo_choose_list;
-    };
 
 
    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
